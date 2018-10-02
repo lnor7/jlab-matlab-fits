d2875dafbd4d79cb62fe5af5a00d291dc96e7ef0
lnor7 <eleanorg1701@gmail.com>
2018-10-02 08:54:35 -0700

    clarifying test example

--- test.m
+++ test.m
@@ -6,7 +6,7 @@ load gauss3.dat;             % Load and prepare the gauss1.txt NIST dataset
 x=gauss3(:,1);
 y=gauss3(:,2);
 
-sig=zeros(size(x))*sqrt(6.25);       % Constant error value, variance (=sigma^2) of gauss3 data set is 6.25
+sig=ones(size(x))*sqrt(6.25);       % Constant error value, variance (=sigma^2) of gauss3 data set is 6.25
 a0 = [94 .009 90.1 113 20 73.8 140 20]; % Starting values for fit
 
 [a,aerr,chisq,res] = chisquare(x,y,sig,nist,{'StartPoint' a0}); % perform fit
